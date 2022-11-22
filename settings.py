7918f67ea102b61c531b0e6a4a1a6b8a9da9c24e
Bearnard Hibbins <bearnard@praekelt.org>
2022-11-22 15:40:51 +0200

    Support filtering contact by group name, Support additional properties, Support hashing urn.

--- settings.py
+++ settings.py
@@ -1,10 +1,13 @@
 IMPORT_FLOW_DATA = True
+HASH_URN = False
 CONTACT_FIELDS = {
     "uuid": "STRING",
     "modified_on": "TIMESTAMP",
     "urn": "STRING",
 }
+ADDITIONAL_CONTACT_PROPERTIES = False  # language, created_on
 GROUP_CONTACT_FIELDS = {"group_uuid": "STRING", "contact_uuid": "STRING"}
+CONTACT_GROUP_FILTER = None
 GROUP_FIELDS = {"name": "STRING", "uuid": "STRING"}
 FLOWS_FIELDS = {"labels": "STRING", "name": "STRING", "uuid": "STRING"}
 FLOW_RUNS_FIELDS = {
